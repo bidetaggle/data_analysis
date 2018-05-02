eb828b9eec083554170611ada581f1f978ad3278
bidetaggle <axel@archlinux.localdomain>
2018-05-02 14:54:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# data #
+
+- dblp.xml - original data
+- extract.xml - shrinked data to make tests (extract from dblp.xml)
+- record.json - record and computation of the data from dblp.xml
+
+# code #
+
+main.js
+
+# install & requirements #
+
+node 10.0.0
+npm 6.0.0
+
+```bash 
+$ npm install
+$ node main.js
+```
---
+++ main.js
@@ -0,0 +1,31 @@
+var bigXml = require('big-xml');
+    
+var reader = bigXml.createReader('dblp.xml', /^(article|mastersthesis|phdthesis)$/, { gzip: false });
+
+var count = [];
+
+var i = 0;
+reader.on('record', function(record) {
+  var recordYear = parseInt(record.attrs.mdate.substring(0,4));
+
+  var foundYear = count.find(function(e){
+  	return e.year == recordYear;
+  });
+
+  let n;
+  if(foundYear == undefined){
+  	n = 1;
+  	count.push({year: recordYear, n:1});
+  }
+  else{
+  	foundYear.n++;
+  	n = foundYear.n;
+  }
+  
+  console.log(`${recordYear} -> ${n}`);
+});
+
+reader.on('end', function(){
+	console.log("The XML record is done :)");
+	console.log(count);
+});
