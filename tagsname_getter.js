5b65f90ca5b18250d8eff9e341531c4cce0c0f86
bidetaggle <axel@archlinux.localdomain>
2018-05-02 23:11:36 +0300

    main tag names getters, children of <dblp>, doesn't work because running out of memory

---
+++ tagsname_getter.js
@@ -0,0 +1,30 @@
+var bigXml = require('big-xml');
+    
+var reader = bigXml.createReader('dblp.xml', /^(dblp)$/, { gzip: false });
+
+var publicationTypes = [];
+
+var output;
+reader.on('record', function(record) {
+  output = record.children;
+
+  //console.log(output);
+
+  for(var i=0 ; i<output.length ; i++ ){
+  	
+  	let publicationTypesStored = publicationTypes.find(function(e){
+	  	return e == output[i].tag;
+	});
+	
+  	if(publicationTypesStored == undefined){
+  		console.log("---"+output[i].tag);
+  		publicationTypes.push(output[i].tag);
+  		console.log(publicationTypes);
+  	}
+  }
+});
+
+reader.on('end', function(){
+	console.log("The XML record is done :)");
+	console.log(publicationTypes);
+});
